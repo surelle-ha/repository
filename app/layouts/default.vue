ab87c133801864e58e1e564727c6ddf7cd5c2e6c
surelle-ha <0110harold@gmail.com>
2026-03-19 16:49:53 +0800

    chore: favicon update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -56,6 +56,8 @@
 const { user, logout: doLogout } = useAuth()
 const { settings, isDocsHidden } = useSiteSettings()
 
+useHead({ bodyAttrs: { class: 'minim-scroll' } })
+
 async function logout() {
   await doLogout()
   navigateTo('/admin/login')
